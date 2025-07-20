2191c6b156d7bfd3b7334aa971ec8381a13d7957
Jhavakar Thillainathan <jhavas4@hotmail.com>
2025-07-20 19:26:59 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Navbar />
         <Routes>
