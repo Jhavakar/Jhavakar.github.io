cd5e112e3d10975a36ed0b6bef96726786696caf
Jhavakar Thillainathan <jhavas4@hotmail.com>
2025-07-20 19:27:37 +0100

    A U

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -13,7 +13,7 @@ const AboutMe = () => {
           </div>
           <div className="about-me-description">
             <p>
-            Innovative and ambitious software developer with over 4s years of experience in web and mobile technologies. I am passionate about solving complex problems and enhancing user experiences through technology. With a proven track record in web, mobile and cross platform development,
+            Innovative and ambitious software developer with over 4 years of experience in web and mobile technologies. I am passionate about solving complex problems and enhancing user experiences through technology. With a proven track record in web, mobile and cross platform development,
              I ensure maximum accessibility, usability, and a seamless user experience. A quick learner and effective communicator, I thrive in collaborative, fast-paced environments. Committed to continuous professional growth, I bring strong problem-solving skills and a keen interest in cutting-edge projects. Open to relocation and new challenges.
             </p>
           </div>
